4dab3a5ca1b5c9b091c3f9313436d935ba5e8a4f
Rahul R Yadav <rryadav2025@gmail.com>
2023-03-06 00:04:53 +0530

    Intro Complete

---
+++ App.js
@@ -0,0 +1,7 @@
+// creating the h1 tag using React
+const heading = React.createElement("h1", {},"Namaste Everyone!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//passing a react element inside the root
+root.render(heading);

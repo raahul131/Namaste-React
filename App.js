b2e78f77593e6e19467f852e9cc39e7d98a73c49
Rahul R Yadav <rryadav2025@gmail.com>
2023-03-09 16:11:49 +0530

    Ignite our app using like parcel...etc

--- App.js
+++ App.js
@@ -1,3 +1,32 @@
+/*
+
+------------- PARCEL -----------------
+
+HMR - hot module Replacement
+File watcher algorithm - file watcher written in c++
+
+- BUNDLING
+- MINIFY
+- Clean or code
+- Dev produnction build
+- Super fast build algorithm
+- Image Optimzation
+- Caching while development
+- It also Compress our code
+- Compitable with older version of Browser also
+- HTTPS on dev machine
+- Parcel also manage port number also\
+- We should push our parcel.cache in git ignore
+- Consistent hashing Algo  
+- Parcel is Zero config
+- 
+
+
+*/
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // creating the h1 tag using React
 const heading = React.createElement("h1", {}, "Namaste Everyone!");
 

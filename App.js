3156215eb6e7db63559c19855ab76028e97acd04
Rahul R Yadav <rryadav2025@gmail.com>
2023-03-10 14:16:35 +0530

    Laying down the foundation

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
 /*
-
 ------------- PARCEL -----------------
-
 HMR - hot module Replacement
 File watcher algorithm - file watcher written in c++
-
 - BUNDLING
 - MINIFY
 - Clean or code
@@ -19,18 +16,44 @@ File watcher algorithm - file watcher written in c++
 - We should push our parcel.cache in git ignore
 - Consistent hashing Algo  
 - Parcel is Zero config
-- 
-
-
 */
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// creating the h1 tag using React
-const heading = React.createElement("h1", {}, "Namaste Everyone!");
+// // creating the h1 tag using React
+// const heading = React.createElement(
+//   "h1",
+//   {
+//     id: "title",
+//     key: "h1",
+//   },
+//   "Namaste Everyone!!!"
+// );
+
+// JSX => React.createElement => Object => HTML(DOM)
+const Heading2 = () => (
+  <h2 id="title" key={"h2"}>
+    Namaste React!!!
+  </h2>
+);
+
+// ----------------  Components  ----------------
+
+const HeaderComponent = () => {
+  return (
+    <div>
+      {<Heading2 />}
+      <h1>Namaste React from funtional Component</h1>
+      <h2>This is h2 tag</h2>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //passing a react element inside the root
-root.render(heading);
+// root.render(heading2);
+
+// to render the functional components
+root.render(<HeaderComponent />);

237f14b7ea6753e55c7f9e0ae35d56690011c909
Rahul R Yadav <rryadav2025@gmail.com>
2023-03-10 18:16:22 +0530

    simple commit

--- App.js
+++ App.js
@@ -1,49 +1,17 @@
-/*
-------------- PARCEL -----------------
-HMR - hot module Replacement
-File watcher algorithm - file watcher written in c++
-- BUNDLING
-- MINIFY
-- Clean or code
-- Dev produnction build
-- Super fast build algorithm
-- Image Optimzation
-- Caching while development
-- It also Compress our code
-- Compitable with older version of Browser also
-- HTTPS on dev machine
-- Parcel also manage port number also\
-- We should push our parcel.cache in git ignore
-- Consistent hashing Algo  
-- Parcel is Zero config
-*/
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// // creating the h1 tag using React
-// const heading = React.createElement(
-//   "h1",
-//   {
-//     id: "title",
-//     key: "h1",
-//   },
-//   "Namaste Everyone!!!"
-// );
-
 // JSX => React.createElement => Object => HTML(DOM)
-const Heading2 = () => (
+const Title = () => (
   <h2 id="title" key={"h2"}>
-    Namaste React!!!
+    Food Villa
   </h2>
 );
-
-// ----------------  Components  ----------------
 
 const HeaderComponent = () => {
   return (
     <div>
-      {<Heading2 />}
+      {<Title />}
       <h1>Namaste React from funtional Component</h1>
       <h2>This is h2 tag</h2>
     </div>
@@ -51,9 +19,6 @@ const HeaderComponent = () => {
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-//passing a react element inside the root
-// root.render(heading2);
 
 // to render the functional components
 root.render(<HeaderComponent />);
